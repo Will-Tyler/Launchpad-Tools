c1b10c469d9d8b53f041b83d27b4f4ccc238904b
Will Tyler <will.tyler11@gmail.com>
2019-01-19 20:31:18 +0000

    Initial Commit

---
+++ Launchpad Tools/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Launchpad Tools
+//
+//  Created by Will Tyler on 1/19/19.
+//  Copyright © 2019 Will Tyler. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
