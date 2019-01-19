09f200dc3237b1edcc687c5ec54ed6dfd8a9acb4
Will Tyler <will.tyler11@gmail.com>
2019-01-19 22:16:57 +0000

    Printing target app titles

--- Launchpad Tools/main.swift
+++ Launchpad Tools/main.swift
@@ -9,12 +9,23 @@
 import Foundation
 
 
-_ = "SELECT rowid, title FROM apps WHERE rowid IN (SELECT rowid FROM items WHERE parent_id = 137 OR parent_id = 138) ORDER BY title ASC LIMIT 35;"
-
 let path = "/var/folders/s5/7swt2cts5xn7fldkx_jlp4jm0000gn/0/com.apple.dock.launchpad/db/db"
 let db = try Connection(path)
 let items = Table("items")
+let apps = Table("apps")
 
-for item in try db.prepare(items) {
-	print(item)
+let title = Expression<String>("title")
+let parentID = Expression<Int64>("parent_id")
+
+let userAppItems = items.select(rowid, parentID).filter(parentID == 137 || parentID == 138)
+var rowIDs = [Int64]()
+
+for item in try db.prepare(userAppItems) {
+	rowIDs.append(item[rowid])
+}
+
+let query = apps.select(rowid, title).filter(rowIDs.contains(rowid)).order(title.asc)
+
+for item in try db.prepare(query) {
+	print(item[title])
 }
